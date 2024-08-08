5ad4cac31de9ff7f695832451b277413ea2d7025
Danny Smith <danny.smith.0@outlook.com>
2024-08-08 09:45:44 -0400

    Made the filename configurable

--- step_challenge.py
+++ step_challenge.py
@@ -2,9 +2,16 @@
 
 import process_step_data
 import plot_step_data
+import sys
 
-step_challenge_data_filename = 'step_challenge_data.csv'
+# Get the filename, either the default one or use the command-line arguement if provided
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    # default filename
+    filename = 'step_challenge_data.csv'
+print('Processing step data from: ' + filename)
 
-df_raw, df_cumulative = process_step_data.process(step_challenge_data_filename)
+df_raw, df_cumulative = process_step_data.process(filename)
 
 plot_step_data.plot_cumulative(df_cumulative)
