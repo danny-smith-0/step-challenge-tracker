fb425704060b12772dd1e9da2bbd6f2eef17540a
Danny Smith <danny.smith.0@outlook.com>
2024-08-12 08:14:04 -0400

    Formatting the main plot
    Added horizontal lines for the y-axis, made the y-axis start at zero, and made the colors stop repeating

--- plot_step_data.py
+++ plot_step_data.py
@@ -2,15 +2,19 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import numpy as np
 
 # plot the cumulative steps each person has walked in the month by day
 def plot_cumulative(df_cumulative):
     # set the figure size
     plt.figure(figsize=(10, 6))
 
+    cmap = plt.get_cmap('jet')
+    colors = cmap(np.linspace(0, 1, len(df_cumulative.columns)))
+
     # Plot each person's steps
-    for person in df_cumulative.columns:
-        plt.plot(df_cumulative.index, df_cumulative[person], label=person, marker='o')
+    for ii, person in enumerate(df_cumulative.columns):
+        plt.plot(df_cumulative.index, df_cumulative[person], label=person, marker='o', color=colors[ii])
 
     # Add labels and title
     plt.xlabel('Date')
@@ -21,6 +25,10 @@ def plot_cumulative(df_cumulative):
     ax = plt.gca()
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%a %m/%d'))  # e.g., "Mon 01/01"
     plt.xticks(rotation=45)
+
+    # More formatting
+    plt.grid(axis='y', color='lightgray', linestyle='-', linewidth=0.7)
+    plt.ylim(bottom = 0)
 
     # Add legend
     plt.legend()
